08fdeb7bf2f5932672d12f1ed8c8fffb73c8e4cf
Rick Walker <ichglauben@gmail.com>
2016-12-04 18:00:38 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+	<Test></Test>
+  </div>
+</template>
+
+<script>
+import Test from './components/test'
+
+export default {
+  name: 'app',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(20,80,150);
+  background: rgb(240, 245, 255);
+  margin-top: 60px;
+  border-radius:10px;
+  padding:1%;
+  box-shadow:5px 5px 15px rgb(20,80,150);
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,66 @@
+<template>
+	<div id="test" class="test">
+		<h3>{{title}}</h3>
+		
+		<label><strong>Title</strong></label> <input type="text" v-model="title" v-on:keyup="presskey">
+		<br/><br/>
+				
+		<p><strong>User:</strong> {{fullName}}</p>
+		
+		<hr/>
+		<br/>
+		<label><strong>First Name</strong></label> <input type="text" v-model="user.fname">
+		<br/><br/>
+		<label><strong>Last Name</strong></label> <input type="text" v-model="user.lname">		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		props: {
+			msg: {
+				type: String,
+				default: 'Default Message'
+			}
+		},
+		data() {
+			return {
+				title: 'Vue Demo App 1',
+				user: {
+					fname: 'Anita',
+					lname: 'Bathe'
+				},
+				showLastName: true,
+				items: [
+					{title:'Item One',},
+					{title:'Item Two',},
+					{title:'Item Fifteen',}
+				]
+			}
+		},
+		methods: {
+			start: function() {
+				var itemCount = this.items.length;
+				alert(itemCount + ' items');
+			},
+			presskey: function(e) {
+				var str = e.target.value;
+				var patt = /^(\s)+$/;
+				
+				this.title = (str.length && !patt.test(str))?str: "Vue Demo App 1";
+			}
+		},
+		computed: {
+			fullName: function() {
+				return this.user.fname + ' ' + this.user.lname;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	div {
+		padding:1%;
+	}
+</style>
